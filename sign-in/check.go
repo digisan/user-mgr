2b14acfd359ceb1227a8ab8baafa59e7d0970559
Qing Miao <digisanqm@outlook.com>
2023-03-01 22:13:52 +1100

    * add frequently check

--- sign-in/check.go
+++ sign-in/check.go
@@ -2,19 +2,76 @@ package signin
 
 import (
 	"fmt"
+	"strings"
+	"sync"
+	"time"
 
 	u "github.com/digisan/user-mgr/user"
 )
 
+var (
+	smAccess = &sync.Map{}
+	fnTS     = func(n int64) int64 {
+		return time.Now().Unix() / n
+	}
+	smFrequently = &sync.Map{}
+)
+
+func recordAccess(uname string, span int) (string, int) {
+	key := fmt.Sprintf("%s-%d", uname, fnTS(int64(span)))
+	if N, ok := smAccess.Load(key); ok {
+		n := N.(int) + 1
+		smAccess.Store(key, n)
+		return key, n
+	}
+	n := 1
+	smAccess.Store(key, n)
+	return key, n
+}
+
+func delAccessRecord(uname string) int {
+	keys := []string{}
+	prefix := uname + "-"
+	smAccess.Range(func(key, value any) bool {
+		if k := key.(string); strings.HasPrefix(k, prefix) {
+			keys = append(keys, k)
+		}
+		return true
+	})
+	for _, key := range keys {
+		smAccess.Delete(key)
+	}
+	return len(keys)
+}
+
+func CheckFrequentlyAccess(uname string, spanSeconds, lockThreshold int) {
+	if _, n := recordAccess(uname, spanSeconds); n >= lockThreshold {
+		smFrequently.Store(uname, struct{}{})
+		return
+	}
+}
+
+func IsFrequentlyAccess(uname string, delay time.Duration) bool {
+	_, ok := smFrequently.Load(uname)
+	if ok {
+		smFrequently.Delete(uname)
+		delAccessRecord(uname)
+	}
+	return ok
+}
+
+//////////////////////////////////////////////////
+
 // if return nil, which means user exists normally
 func UserStatusIssue(login *u.User) error {
-	if u.UserExists(login.UName, login.Email, true) {
+	uname, email := login.UName, login.Email
+	if u.UserExists(uname, email, true) {
 		return nil
 	}
-	if u.UserExists(login.UName, login.Email, false) {
-		return fmt.Errorf("[%v] is dormant", login.UName)
+	if u.UserExists(uname, email, false) {
+		return fmt.Errorf("[%v] is dormant, cannot login", uname)
 	}
-	return fmt.Errorf("[%v] is not existing", login.UName)
+	return fmt.Errorf("[%v] is not existing", uname)
 }
 
 // if successful, then update login user
--- sign-in/trail_test.go
+++ sign-in/trail_test.go
@@ -52,3 +52,43 @@ func TestOfflineMonitor(t *testing.T) {
 
 	time.Sleep(1 * time.Minute)
 }
+
+func TestTimeStamp(t *testing.T) {
+
+	fmt.Println(recordAccess("qmiao", 3))
+	time.Sleep(1 * time.Second)
+	fmt.Println(recordAccess("qmiao", 3))
+	time.Sleep(2 * time.Second)
+	fmt.Println(recordAccess("qmiao", 4))
+
+	fmt.Println("---------")
+
+	delAccessRecord("qmiao")
+
+	smAccess.Range(func(key, value any) bool {
+		fmt.Println(key, value)
+		return true
+	})
+}
+
+func TestLockCheck(t *testing.T) {
+	const USER = "test"
+	uname := USER
+
+	for i := 0; i < 20; i++ {
+
+		fmt.Printf("%s trying... %02d\n", uname, i)
+
+		if IsFrequentlyAccess(uname, 5*time.Second) {
+			fmt.Printf("locked, wait %v\n", 5*time.Second)
+			time.Sleep(5 * time.Second)
+		}
+
+		CheckFrequentlyAccess(uname, 3, 6)
+
+		// different user is not blocked.
+		// uname = fmt.Sprintf("%s:%d", USER, i)
+
+		time.Sleep(100 * time.Millisecond)
+	}
+}
